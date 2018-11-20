64c95d2c6d3630bca04358f99de4f381dcc28285
Janine Luk <janineluk95@gmail.com>
2018-11-20 19:23:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
   def initialize(dog_name, dog_breed = "Mutt")
-    @dog_name = dog_name
-    @dog_breed = dog_breed
+    @name = dog_name
+    @breed = dog_breed
   end
+  def name,
 end
