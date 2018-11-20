e884114760acd3e2f64b2910657d2ec1c3378e6d
Janine Luk <janineluk95@gmail.com>
2018-11-20 19:24:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,11 @@ class Dog
     @breed = dog_breed
   end
   def name = (dog_name, dog_breed = "Mutt")
-    
+    @name = dog_name
+    @breed = dog_breed
+  end
+  def name = (name, breed)
+    @name = name
+    @breed = breed
   end
 end
